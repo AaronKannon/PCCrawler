1674ba68ae35d62397cab247cf7c5e01eec3f8cd
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-10-26 21:31:17 -0300

    Fixing the get attributes method

--- core/deal_report.py
+++ core/deal_report.py
@@ -6,34 +6,34 @@ class DealReport:
     def __init__(self, boxes_section_element):
         self.boxes_section_element = boxes_section_element
         self.boxes = self.pull_deal_boxes()
-        print(self.boxes)
 
     def pull_deal_boxes(self):
-        print('yeay1')
-        return self.boxes_section_element.find_elements_by_class_name(
-            'sc-TBWPX gifNHa cardProdutoListagem'
+        return self.boxes_section_element.find_elements_by_css_selector(
+            'div[class="sc-TBWPX gifNHa cardProdutoListagem"]'
         )
 
     def pull_deal_box_attributes(self):
         collection = []
-        print('yeay2')
         for box in self.boxes:
-            print(box)
             # Pulling the hotel name
             link = box.find_element_by_tag_name('a')
-            print(link)
-            data = link.find_element_by_class_name('sc-eLwHnm hGtoqp')
-            print(data)
-            product_name = data.find_element_by_class_name(
-                'sc-jObWnj imnAbk'
+            data_name = link.find_element_by_css_selector(
+                'div[class="sc-eLwHnm hGtoqp"]'
+                )
+            data_price = link.find_element_by_css_selector(
+                'div[class="sc-bTfYFJ bBqvgl"]'
+                )
+            product_name = data_name.find_element_by_css_selector(
+                'h2[class="sc-jObWnj imnAbk"]'
             ).get_attribute('innerHTML').strip()
-            product_price = data.find_element_by_class_name(
-                'sc-iwjdpV ezjpDw priceCard'
+            product_price = data_price.find_element_by_css_selector(
+                'span[class="sc-iwjdpV ezjpDw priceCard"]'
             ).get_attribute('innerHTML').strip()
-            product_brand = data.find_element_css_selector('span').strip()
+            product_brand = data_name.find_element_by_css_selector('span').get_attribute(
+                'innerHTML'
+            ).strip()
 
             collection.append(
                 [product_name, product_price, product_brand]
             )
-            print('yeay3')
         return collection
